96ac01bb10ebde404d710f59066457ae40b2892d
xjahod06 <jahoda.vojta@gmail.com>
2020-04-12 12:44:25 +0200

    zaklady, lcd ukazatel

---
+++ mainwindow.cpp
@@ -0,0 +1,18 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->horizontalSlider, SIGNAL(valueChanged(int)), ui->lcdNumber, SLOT(display(int)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
